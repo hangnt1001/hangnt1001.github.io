1c97b5566f00fc52827617e21ac297e5056ec4bc
Harry Nguyen <gluesolution@gmail.com>
2025-05-19 11:22:01 +0700

    feat: redesign my landing page

--- _plugins/cdn.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module AssetFilter
-    def cdn(input)
-      "#{@context.registers[:site].config['cdn_url']}#{input}"
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::AssetFilter)
